73c110722edd8c2f21522088c385c83a3b164d81
Лукьянов Александр <lukyanov_av@st-inv.ru>
2023-04-27 12:49:59 +0300

    adding basic functionality

--- weather.js
+++ weather.js
@@ -1,7 +1,17 @@
 #!usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import {
+	printHelp,
+	printSuccess,
+	printError,
+	printWeather,
+} from "./services/log.service.js";
+import {
+	saveKeyValue,
+	TOKEN_DICTIONARY,
+	getKeyValue,
+} from "./services/storage.service.js";
+import { getWeather, getIcon } from "./services/api.service.js";
 
 const saveToken = async (token) => {
 	if (token.lenght == false) {
@@ -9,10 +19,44 @@ const saveToken = async (token) => {
 		return;
 	}
 	try {
-		await saveKeyValue("token", token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess("Token was saved");
 	} catch (error) {
 		printError(error.message);
+	}
+};
+const saveCity = async (city) => {
+	if (city.lenght == false) {
+		printError("No city data");
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		const weather = await getWeather();
+		if (weather.cod == 200) {
+			printSuccess("City was saved");
+		}
+	} catch (error) {
+		printError(error.message);
+	}
+};
+
+const getForcast = async () => {
+	try {
+		const city =
+			process.env.CITY ??
+			(await getKeyValue(TOKEN_DICTIONARY.city)) ??
+			"london";
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
+	} catch (error) {
+		if (error?.response?.status == 404) {
+			printError("wrong city");
+		} else if (error?.response?.status == 401) {
+			printError("not valid token");
+		} else {
+			printError(error.message);
+		}
 	}
 };
 
@@ -20,14 +64,14 @@ const initCLI = () => {
 	const args = getArgs(process.argv);
 
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
-		// city
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
+	return getForcast();
 };
-
 initCLI();
